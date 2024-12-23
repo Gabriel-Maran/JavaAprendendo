4ee55a23d66f3dbb43038c1526ae5a6a9080b6a1
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-12-22 22:15:10 -0300

    aprendendo objetos - 7

---
+++ src/aprendendoObjetos/modificadorestatico/exercicio/dominio/Anime.java
@@ -0,0 +1,33 @@
+package aprendendoObjetos.modificadorestatico.exercicio.dominio;
+
+public class Anime {
+    private String nome;
+    private String genero;
+    private static int[] episodios;
+    private static int contadorObjetos = 0;
+    static {
+        episodios = new int[100];
+        for(int i = 0; i<episodios.length; i++){
+            episodios[i] = i+1;
+        }
+    }
+
+    public Anime(String nome, String genero) {
+        this.nome = nome;
+        this.genero = genero;
+        contadorObjetos++;
+    }
+
+    public static int getContadorObjetos() {
+        return contadorObjetos;
+    }
+    public static void exibirEpisodiosPares(){
+        System.out.println("Episodios pares: ");
+        for(int episodio : episodios) {
+            if (episodio % 2 == 0) {
+                System.out.print(episodio+" ");
+            }
+        }
+        System.out.println();
+    }
+}
