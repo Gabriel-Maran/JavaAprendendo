1b3f61f9031f0357c72d1852193de8d33a870e78
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-12-22 21:39:57 -0300

    aprendendo objetos - 6

--- src/aprendendoObjetos/modificadorestatico/dominio/Anime.java
+++ src/aprendendoObjetos/modificadorestatico/dominio/Anime.java
@@ -1,28 +1,37 @@
-package aprendendoObjetos.blocosinicializacao.dominio;
+package aprendendoObjetos.modificadorestatico.dominio;
 
 public class Anime {
     private String nome;
-    private int[] episodios;
+    private static int[] episodios;
     //Ordem seguida:
+    //0- Bloco de inicialização é executado quando a JVM carregar a classe
     //1- É alocado em espaço de memoria o objeto
     //2- Cada atributo de classe é criado com um valor default ou outro que foi passado
     //3- Bloco de inicialização é executado
     //4- Contrutor é executado
-    {
-        System.out.println("Dentro do bloco de inicialização");
+    static {
+        System.out.println("Dentro do bloco de inicialização estático 1");
         //Bloco de inicialização, ele sempre é inicializado antes dos contrutores e em todos os objetos
         episodios = new int[100];
         for (int i = 0; i < episodios.length; i++) {
             episodios[i] = i + 1;
         }
     }
-
+    static {
+        System.out.println("Dentro do bloco de inicialização estático 2");
+    }
+    static {
+        System.out.println("Dentro do bloco de inicialização estático 3");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização não estático 1");
+    }
     public Anime(String nome) {
         this.nome = nome;
     }
 
     public Anime() {
-        for (int episodios : this.episodios) {
+        for (int episodios : Anime.episodios) {
             System.out.print(episodios + " ");
         }
         System.out.println();
