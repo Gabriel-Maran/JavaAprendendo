7ffff62d28e70a081c48ea226b943c59e56d4549
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-12-23 13:51:26 -0300

    aprendendo objetos - 8

---
+++ src/aprendendoObjetos/associacao/exercicio/teste/JogadoresTeste01.java
@@ -0,0 +1,29 @@
+package aprendendoObjetos.associacao.exercicio.teste;
+
+import aprendendoObjetos.associacao.exercicio.dominio.Jogador;
+import aprendendoObjetos.associacao.exercicio.dominio.Time;
+
+public class JogadoresTeste01 {
+    public static void main(String[] args) {
+        Time time1 = new Time("Argentina");
+        Time time2 = new Time("Alemanha");
+        Jogador jogador1 = new Jogador("Messi");
+        Jogador jogador2 = new Jogador("Di María");
+        Jogador jogador3 = new Jogador("Kroos");
+        Jogador jogador4 = new Jogador("Neuer");
+        Jogador[] jogadoresArg = {jogador1, jogador2};
+        Jogador[] jogadoresAlm = {jogador3, jogador4};
+        jogador1.setTime(time1);
+        jogador2.setTime(time1);
+        jogador3.setTime(time2);
+        jogador4.setTime(time2);
+        time1.setJogadores(jogadoresArg);
+        time2.setJogadores(jogadoresAlm);
+        jogador1.imprime();
+        jogador2.imprime();
+        jogador3.imprime();
+        jogador4.imprime();
+        time1.imprime();
+        time2.imprime();
+    }
+}
--- src/aprendendoObjetos/modificadorestatico/exercicio/teste/TesteAnime01.java
+++ src/aprendendoObjetos/modificadorestatico/exercicio/teste/TesteAnime01.java
@@ -7,7 +7,7 @@ public class TesteAnime01 {
     public static void main(String[] args) {
         Anime anime1 = new Anime("Anime1", "Genero1");
         Anime anime2 = new Anime("Anime2", "Genero2");
-        Anime anime3 = new Anime("Anime3", "Genero3");
+        Anime   anime3 = new Anime("Anime3", "Genero3");
         Anime.exibirEpisodiosPares();
         Anime.exibirEpisodiosPares();
         Anime.exibirEpisodiosPares();
