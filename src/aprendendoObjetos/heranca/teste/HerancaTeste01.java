380586751f9631d5a7b2177034453b63943dd8ca
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-12-25 22:24:04 -0300

    aprendendo objetos - 11

---
+++ src/aprendendoObjetos/heranca/teste/HerancaTeste01.java
@@ -0,0 +1,27 @@
+package aprendendoObjetos.heranca.teste;
+
+import aprendendoObjetos.heranca.dominio.Endereco;
+import aprendendoObjetos.heranca.dominio.Funcionario;
+import aprendendoObjetos.heranca.dominio.Pessoa;
+
+public class HerancaTeste01 {
+    public static void main(String[] args) {
+        Endereco endereco1 =  new Endereco();
+        endereco1.setRua("Rua 1");
+        endereco1.setCep("012345-678");
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setNome("Pessoa 1");
+        pessoa1.setCpf("11111111111");
+        pessoa1.setEndereco(endereco1);
+        pessoa1.imprime();
+        System.out.println("-------------------------------");
+
+        Funcionario funcionario1 = new Funcionario();
+        funcionario1.setNome("Funcionario 1");
+        funcionario1.setCpf("22222222222");
+        funcionario1.setEndereco(endereco1);
+        funcionario1.setSalario(20000);
+
+        funcionario1.imprime();
+    }
+}
