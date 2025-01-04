292f3600e9c162e3013234e91f94fc69fbdeb1fe
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-01-04 14:29:04 -0300

    aprendendo exceções - 04

---
+++ src/aprendendoExcecoes/runtime/teste/RunTimeExceptionTeste03.java
@@ -0,0 +1,20 @@
+package aprendendoExcecoes.runtime.teste;
+
+public class RunTimeExceptionTeste03 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static void abreConexao() {
+        //Pode ser try e catch; try e finally; try, catch e finally
+        try {
+            System.out.println("Abrindo arquivo");
+            throw new RuntimeException();
+//            System.out.println("Escrevendo dados no arquivo");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
