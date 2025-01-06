65fed655202a8faa2a90ff3c17a66874c8028867
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-01-05 21:44:36 -0300

    aprendendo exceções - 04

--- src/aprendendoExcecoes/exception/teste/ExceptionTeste02.java
+++ src/aprendendoExcecoes/exception/teste/ExceptionTeste02.java
@@ -16,13 +16,13 @@ public class ExceptionTeste02 {
         File file = new File("src/aprendendoExcecoes/exception/arquivo\\teste.txt");
         //Se for proibido a criação de arquivos dentro daquela pasta ou local, não será possivel criar e vai dar o IOException de acesso negado
         try{
-            boolean isCriado = file.createNewFile();
+            boolean isCriado = file.createNewFile(); //V ou F
             System.out.println("Arquivo criado com sucesso!");
         }catch(IOException exception){//ao inves de "exception" pode se escrever "e" e "ex"
-            //Nunca deixar esse bloco em branco
+            //Nunca deixar esse bloco em branco, pois se deixar em branco, não saberá qual foi o problema
             exception.printStackTrace();
             // throw new RuntimeException("Problema na hora de criar o arquivo"); // Outro metodo, indo de exception em exception
             throw exception; //Relança a exceção
-        }
+        } //Pode ser adicionado o bloco finally para executar, mesmo se o catch não ser executado, o finally será
     }
 }
