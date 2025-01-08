f1c2d3717455d131c9d6636be5443fb3a462842a
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-01-08 18:04:04 -0300

    aprendendo exceções - 05

---
+++ src/aprendendoExcecoes/exception/teste/TryWithResourceTeste01.java
@@ -0,0 +1,38 @@
+package aprendendoExcecoes.exception.teste;
+
+import aprendendoExcecoes.exception.dominio.Leitor01;
+import aprendendoExcecoes.exception.dominio.Leitor02;
+
+import java.io.*;
+
+public class TryWithResourceTeste01 {
+    public static void main(String[] args) {
+        lerArquivo();
+    }
+
+    // melhor jeito
+    public static void lerArquivo() {
+        try (Leitor01 leitor01 = new Leitor01();
+             Leitor02 leitor02 = new Leitor02()) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // jeito 2
+    public static void lerArquivo2() {
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("teste.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
