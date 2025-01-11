ab6ffc59eb33d0fe79f6ca4bad6edf3bff4b860c
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-01-11 14:38:32 -0300

    aprendendo classes utilitárias  - 02

---
+++ src/aprendendoClassesUtilitarias/string/teste/StringBuilderTeste01.java
@@ -0,0 +1,18 @@
+package aprendendoClassesUtilitarias.string.teste;
+
+public class StringBuilderTeste01 {
+    public static void main(String[] args) {
+        String nome = "Gabriel";
+        nome.concat(" Maran");
+        System.out.println(nome);
+        StringBuilder sb = new StringBuilder(16);
+        sb.append(nome);
+        sb.append(" 1").append(" 2");
+        System.out.println(sb.toString());
+        sb.reverse();
+        System.out.println(sb.toString());
+        sb.reverse();
+        sb.delete(0,3);
+        System.out.println(sb.toString());
+    }
+}
