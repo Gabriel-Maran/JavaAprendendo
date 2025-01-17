fb86efbf85399c37a6833de3e5000dfa27758864
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-01-16 22:03:28 -0300

    aprendendo classes utilitárias  - 07

---
+++ src/aprendendoClassesUtilitarias/datas/teste/DatasTeste01.java
@@ -0,0 +1,21 @@
+package aprendendoClassesUtilitarias.datas.teste;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class DatasTeste01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("Digite sua data de nascimento: (dd/MM/yyyy)");
+            String data = sc.nextLine();
+            DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate dataAniversario = LocalDate.parse(data, formatador);
+            System.out.println("O aniversario é: " + dataAniversario);
+        }catch (DateTimeParseException e){
+            e.printStackTrace();
+        }
+    }
+}
