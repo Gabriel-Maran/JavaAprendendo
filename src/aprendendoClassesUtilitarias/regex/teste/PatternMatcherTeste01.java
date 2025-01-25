4b21ceb03f4b04d3807d782d67a040d25bb215af
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-01-24 22:07:41 -0300

    aprendendo classes utilitárias  - 10

--- src/aprendendoClassesUtilitarias/regex/teste/PatternMatcherTeste01.java
+++ src/aprendendoClassesUtilitarias/regex/teste/PatternMatcherTeste01.java
@@ -6,16 +6,19 @@ import java.util.regex.Pattern;
 public class PatternMatcherTeste01 {
     public static void main(String[] args) {
         String regex = "aba";
-        String texto = "abaaba";
-        String texto2 = "abababa";
+        String texto = "abababa";
+        boolean encontrou = false;
         Pattern pattern = Pattern.compile(regex);
-        Matcher matchre = pattern.matcher(texto2);
-        System.out.println("texto: " + texto2);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto: " + texto);
         System.out.println("indice: 0123456789");
         System.out.println("regex: " + regex);
         System.out.println("Posições encontradas");
-        while(matchre.find()) {
-            System.out.print(matchre.start()+" ");
+        while(matcher.find()) {
+            System.out.print(matcher.start()+" ");
+        }
+        if(!encontrou) {
+            System.out.println("Nenhuma posição encontrada");
         }
     }
 }
