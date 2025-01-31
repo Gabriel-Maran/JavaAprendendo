7cb362a458f0cafc9000d72a627295de2a08dba1
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-01-30 21:10:41 -0300

    aprendendo classes utilitárias  - 16

---
+++ src/aprendendoClassesUtilitarias/nio/teste/BasicFileAttributesTeste02.java
@@ -0,0 +1,33 @@
+package aprendendoClassesUtilitarias.nio.teste;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
+public class BasicFileAttributesTeste02 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Users\\Gabriel\\Desktop\\JavaAprendendo\\src\\aprendendoClassesUtilitarias\\nio\\teste\\arquivo\\new.txt");
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+        FileTime creationTime = basicFileAttributes.creationTime();
+        FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
+        FileTime lastAccessTime = basicFileAttributes.lastAccessTime();
+        System.out.println("Tempo de criação:" + creationTime);
+        System.out.println("Ultima Modificação:" +lastModifiedTime);
+        System.out.println("Ultimo Acesso:" +lastAccessTime);
+        System.out.println("-------------------------------------");
+
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime fileTimeNow = FileTime.fromMillis(System.currentTimeMillis());
+        fileAttributeView.setTimes(lastModifiedTime, fileTimeNow, creationTime);
+
+        creationTime = fileAttributeView.readAttributes().creationTime();
+        lastModifiedTime = fileAttributeView.readAttributes().lastModifiedTime();
+        lastAccessTime = fileAttributeView.readAttributes().lastAccessTime();
+        System.out.println("Tempo de criação:" + creationTime);
+        System.out.println("Ultima Modificação:" +lastModifiedTime);
+        System.out.println("Ultimo Acesso:" +lastAccessTime);    }
+}
