218f8ca90cff6aaebaea32a1f49300ecd3898e6f
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-02-11 15:01:34 -0300

    aprendendo Generics - 2

---
+++ src/aprendendoGenerics/dominio/Carro.java
@@ -0,0 +1,16 @@
+package aprendendoGenerics.dominio;
+
+public class Carro {
+    private String nome;
+
+    public Carro(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
