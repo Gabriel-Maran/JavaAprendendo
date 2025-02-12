8668e32e9b010389df6a22f6a4384c01c13489f2
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-02-12 16:11:19 -0300

    aprendendo Generics - 3

---
+++ src/aprendendoGenerics/teste/MetodoGenericoTeste01.java
@@ -0,0 +1,22 @@
+package aprendendoGenerics.teste;
+
+import aprendendoGenerics.dominio.Barco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetodoGenericoTeste01 {
+    public static void main(String[] args) {
+        List<Barco> barcos = List.of(new Barco("Yate"), new Barco("Canoqa"));
+        List<List<Barco>> listBarco = criarArrayComUmObjeto(barcos);
+        System.out.println(listBarco);
+    }
+
+    private static <T> List<T> criarArrayComUmObjeto(T t){
+        return List.of(t);
+    }
+//    private static <T> void criarArrayComUmObjeto(T t){ //metodo sem retorno
+//        System.out.println(List.of(t));
+//    }
+}
+
