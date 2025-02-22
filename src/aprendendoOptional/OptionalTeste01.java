fa596d78ef25df444181523a9d57f51b8abee2d1
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-02-22 19:17:30 -0300

    aprendendo Optional - 01

---
+++ src/aprendendoOptional/OptionalTeste01.java
@@ -0,0 +1,33 @@
+package aprendendoOptional;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalTeste01 {
+    public static void main(String[] args) {
+        Optional<String> o1 = Optional.of("Frase foda");
+        Optional<String> o2 = Optional.ofNullable(null);
+        Optional<String> o3 = Optional.empty();
+
+        System.out.println(o1);
+        System.out.println(o2);
+        System.out.println(o3);
+        Optional<String> findGabriel = findName("Gabriel");
+        System.out.println(findGabriel);
+        Optional<String> findJorge = findName("Jorge");
+        System.out.println(findJorge);
+//        Optional<String> nameOptional = Optional.ofNullable(findName("Gabriel"));
+//        String isEmpty = nameOptional.orElse("EMPTY");
+//        nameOptional.ifPresent(s-> System.out.println(s.toUpperCase()));
+//        System.out.println(isEmpty);
+    }
+
+    private static Optional<String> findName(String name) {
+        List<String> nomes = List.of("Gabriel", "João", "Cleiton", "Taffe");
+        int i = nomes.indexOf(name);
+        if(i != -1){
+            return Optional.of(nomes.get(i));
+        }
+        return Optional.empty();
+    }
+}
