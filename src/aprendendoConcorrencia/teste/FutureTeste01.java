e4a53cbf89e6215062bb6051288468a895607fcb
GabrielMaran <gabriel.enzo.libero.maran@gmail.com>
2025-03-27 16:44:13 -0300

    aprendendo sobre concorrecia - 07

---
+++ src/aprendendoConcorrencia/teste/FutureTeste01.java
@@ -0,0 +1,32 @@
+package aprendendoConcorrencia.teste;
+
+import java.util.concurrent.*;
+
+public class FutureTeste01 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Double> dollar = executorService.submit(new Callable<Double>() {
+            @Override
+            public Double call() throws Exception {
+                TimeUnit.SECONDS.sleep(2);
+                return 4.36D;
+            }
+        });
+        System.out.println(doSomething());
+        try {
+            System.out.println("Dollar: "+dollar.get(3, TimeUnit.SECONDS));
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new RuntimeException("Erro ao pegar o valor ");
+        }
+        executorService.shutdown();
+    }
+
+    private static long doSomething() {
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
