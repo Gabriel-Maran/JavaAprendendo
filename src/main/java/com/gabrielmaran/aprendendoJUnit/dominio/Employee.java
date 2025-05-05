35303c0fe9c24e85a763b916699091846da3dfe7
GabrielMaran <gabriel.enzo.libero.maran@gmail.com>
2025-05-05 12:54:44 -0300

    Aprendendo Java, 285 aulas finalizadas - DevDojo

---
+++ src/main/java/com/gabrielmaran/aprendendoJUnit/dominio/Employee.java
@@ -0,0 +1,13 @@
+package com.gabrielmaran.aprendendoJUnit.dominio;
+
+public class Employee {
+    private final String id;
+
+    public Employee(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
